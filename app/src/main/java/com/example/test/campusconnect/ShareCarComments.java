7573fa90009503e751be7aaa4d786343b9bd7ad0
samsony17 <sams.sony@gmail.com>
2015-11-01 16:36:17 -0600

    gps share location

--- app/src/main/java/com/example/test/campusconnect/ShareCarComments.java
+++ app/src/main/java/com/example/test/campusconnect/ShareCarComments.java
@@ -1,23 +1,43 @@
 package com.example.test.campusconnect;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.Address;
 import android.location.Geocoder;
+import android.os.AsyncTask;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 public class ShareCarComments extends AppCompatActivity {
-    String addresss,citys,states,countrys,username,postid,flag;
+    String addresss,citys,states,countrys,username,postid,Flag;
     static String EXTRA_ADDRESS = "com.example.test.addresss";
     static String EXTRA_CITY=     "com.example.test.citys";
     static String EXTRA_STATE=    "com.example.test.states";
@@ -30,6 +50,9 @@ public class ShareCarComments extends AppCompatActivity {
     Button btnconfirmUnjoinY;
     Button btnconfirmUnjoinN;
     EditText txtComment;
+    SessionManager session;
+    private AsyncDataClass asyncRequestObject;
+    private final String serverUrlComment = configuration.URL_COMMENTS;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,13 +65,14 @@ public class ShareCarComments extends AppCompatActivity {
         txtConfirmUnjoin=(TextView) findViewById(R.id.UjConfirmation);
         btnconfirmUnjoinN=(Button) findViewById(R.id.btnUjConfirmNo);
         postid="";
-        flag="0";
+        Flag="0";
+
         final Bundle extras=getIntent().getExtras();
         if(extras!=null){
             postid = extras.getString("post_id");
-            flag = extras.getString("flag");
+            Flag = extras.getString("flag");
         }
-        if(flag=="0"){
+        if(Flag=="0"){
             btnShareLocation.setVisibility(View.GONE);
             txtComment.setVisibility(View.GONE);
             location.setVisibility(View.GONE);
@@ -57,6 +81,7 @@ public class ShareCarComments extends AppCompatActivity {
             btnconfirmUnjoinY.setVisibility(View.VISIBLE);
             btnconfirmUnjoinN.setVisibility(View.VISIBLE);
         }
+
         btnconfirmUnjoinY.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -76,6 +101,17 @@ public class ShareCarComments extends AppCompatActivity {
         btnSendCarReq.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                session = new SessionManager(getApplicationContext());
+                session.checkLogin();
+                HashMap<String,String> user = session.getUserDetails();
+                username = user.get(SessionManager.KEY_EMAIL);
+                String enteredText = txtComment.getText().toString();
+                AsyncDataClass asyncRequestObject = new AsyncDataClass();
+                asyncRequestObject.execute(serverUrlComment, enteredText, username, location.getText().toString(), Flag);
+                Toast.makeText(ShareCarComments.this, "Posted Succesfully", Toast.LENGTH_LONG).show();
+                Intent doneIntent=new Intent(getBaseContext(),ShareCarComments.class);
+                doneIntent.putExtra("post_id","flag");
+                startActivity(doneIntent);
 
                 if(location.getText().toString()!="location"){
                     //txtComment.getText().toString();
@@ -116,7 +152,9 @@ public class ShareCarComments extends AppCompatActivity {
         }
 
     }
-    public void onNavigate(View view) {
+
+
+public void onNavigate(View view) {
 
         Intent intent = new Intent(this,MapsActivity.class);
         intent.putExtra(EXTRA_ADDRESS, addresss);
@@ -125,6 +163,140 @@ public class ShareCarComments extends AppCompatActivity {
         intent.putExtra(EXTRA_COUNTRY, countrys); // getText() SHOULD NOT be static!!!
         startActivity(intent);
     }
+
+
+    private class AsyncDataClass extends AsyncTask<String, Void, String> {
+
+        @Override
+
+        protected String doInBackground(String... params) {
+
+            String jsonResult = "";
+
+            try {
+
+                Map<String,String> nameValuePairs = new HashMap<String,String>();
+                nameValuePairs.put("comment", params[1]);
+                nameValuePairs.put("username",params[2]);
+                nameValuePairs.put("location",params[3]);
+                nameValuePairs.put("Flag",params[4]);
+
+                URL url = new URL(serverUrlComment);
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.setDoOutput(true);
+                con.setDoInput(true);
+                con.setRequestMethod("POST");
+                con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+                try {
+                    OutputStreamWriter writer = new OutputStreamWriter(con.getOutputStream());
+                    //OutputStream os = con.getOutputStream();
+                    //BufferedWriter writer = new BufferedWriter( new OutputStreamWriter(os, "UTF-8" ) );
+                    writer.write(getQuery(nameValuePairs));
+                    writer.flush();
+                    writer.close();
+                } catch (Exception e) {
+                    System.out.print(e);
+
+                }
+                con.connect();
+
+                //Encoded String
+                //String encodedStr = getEncodedData(nameValuePairs);
+                //writer.write(get);
+                //writer.flush();
+
+
+                InputStreamReader in = new InputStreamReader(con.getInputStream());
+                jsonResult = inputStreamToString(in).toString();
+                System.out.print(jsonResult);
+
+
+            } catch (Exception e) {
+
+                e.printStackTrace();
+                Log.e("MYAPP", "exception", e);
+
+            }
+
+            return jsonResult;
+
+        }
+
+        private String getQuery(Map<String, String> nameValuePairs) throws UnsupportedEncodingException {
+            StringBuilder result = new StringBuilder();
+            boolean first = true;
+            for (Map.Entry<String, String> pair : nameValuePairs.entrySet()) {
+                if (first)
+                    first = false;
+                else result.append("&");
+                result.append(URLEncoder.encode(pair.getKey(), "UTF-8"));
+                result.append("=");
+                result.append(URLEncoder.encode(pair.getValue(), "UTF-8"));
+            }
+            return result.toString();
+        }
+
+        //
+        @Override
+
+        protected void onPreExecute() {
+
+            super.onPreExecute();
+
+        }
+
+        @Override
+
+        protected void onPostExecute(String result) {
+
+            super.onPostExecute(result);
+            //Toast.makeText(Login.this,result, Toast.LENGTH_LONG).show();
+
+            System.out.println("Resulted Value: " + result);
+
+            if (result.equals("") || result == null) {
+
+                Toast.makeText(ShareCarComments.this, "Server Connection Fail", Toast.LENGTH_LONG).show();
+
+                return;
+
+            }
+
+        }
+
+
+        private StringBuilder inputStreamToString(InputStreamReader is) {
+
+            String rLine = "";
+
+            StringBuilder answer = new StringBuilder();
+
+            BufferedReader br = new BufferedReader(is);
+
+            try {
+
+                while ((rLine = br.readLine()) != null) {
+
+                    answer.append(rLine);
+
+                }
+
+            } catch (IOException e) {
+
+                // TODO Auto-generated catch block
+
+                e.printStackTrace();
+
+            }
+
+            return answer;
+
+        }
+
+    }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
--- app/src/main/java/com/example/test/campusconnect/configuration.java
+++ app/src/main/java/com/example/test/campusconnect/configuration.java
@@ -16,5 +16,6 @@ public class configuration {
     public static String URL_SHARE_RIDE = "http://ec2-52-21-243-105.compute-1.amazonaws.com/poolpost.php" ;
     public static String URL_MY_RIDES = "http://ec2-52-21-243-105.compute-1.amazonaws.com/userpostlist.php" ;
     public static String URL_ALL_RIDES = "http://ec2-52-21-243-105.compute-1.amazonaws.com/postlist.php" ;
+    public static String URL_COMMENTS = "http://ec2-52-21-243-105.compute-1.amazonaws.com/comment.php" ;
 
 }
